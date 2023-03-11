58d91ec6ec27aa449a9a396efc824e79f9b41e70
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-03-12 01:52:07 +0500

    Add more UI stuff

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Globals
+let time = 5;
+let score = 0;
+let isPlaying;
+
+let wordInput = document.querySelector('#wordInput');
+let currentWord = document.querySelector('#currentWord');
+let seconds = document.querySelector('#seconds');
+let message = document.querySelector('#message');
